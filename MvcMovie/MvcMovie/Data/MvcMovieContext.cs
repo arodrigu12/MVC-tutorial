c9bf7159bae92e695cc489726acf53aab88fff1d
Alfredo Rodriguez <arodrigu12@hotmail.com>
2020-01-01 18:43:36 -0500

    Completed 'Add a model' section of MVC tutorial

---
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        //Entity set property corresponds to db table, 
+        //while an entity corresponds to a row in the table
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
